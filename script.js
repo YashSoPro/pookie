364aef1f48cf950bb003ce43ead2136d1aaabdcc
SableMeow <105553471+YashSoPro@users.noreply.github.com>
2025-02-14 00:42:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ document.addEventListener("DOMContentLoaded", function () {
     letterCover.addEventListener("click", function () {
         letterCover.style.display = "none";
         letter.style.display = "block";
+
+        // Play background music when letter opens
+        document.getElementById("bgMusic").play();
     });
 
     // Buttons Interaction
@@ -34,36 +37,4 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     setInterval(createHeart, 500);
-});        setTimeout(() => {
-            heart.remove();
-        }, 4000);
-    }
-
-    setInterval(createFloatingHearts, 500);
-
-    // Floating Text on Button Click
-    function createFloatingText(text, button) {
-        let floatingText = document.createElement("div");
-        floatingText.innerText = text;
-        floatingText.style.position = "absolute";
-        floatingText.style.color = "red";
-        floatingText.style.fontSize = "16px";
-        floatingText.style.fontWeight = "bold";
-        floatingText.style.left = button.offsetLeft + button.offsetWidth / 2 + "px";
-        floatingText.style.top = button.offsetTop - 20 + "px";
-        floatingText.style.animation = "floatUpText 1.5s ease-out";
-
-        document.body.appendChild(floatingText);
-        setTimeout(() => floatingText.remove(), 1500);
-    }
-
-    // Play background music (Ambarsariya)
-    var sound = new Howl({
-        src: ['https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3'],
-        autoplay: true,
-        loop: true,
-        volume: 0.5,
-    });
-
-    sound.play();
 });
