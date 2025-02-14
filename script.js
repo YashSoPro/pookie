d3124a8bf3a75df1e5226ce6821e9bfb3676e7e1
SableMeow <105553471+YashSoPro@users.noreply.github.com>
2025-02-14 19:13:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad... 😢",
-    "I will be very sad... 😞",
-    "I will be very very very sad... 💔",
-    "Ok fine, I will stop asking...",
+    "Are you sure? 💔",
+    "Really sure?? 😢",
+    "Are you positive? 😭",
+    "Pookie please... 🥺",
+    "Just think about it! 💕",
+    "If you say no, I will be really sad... 😞",
+    "I will be very sad... 😔",
+    "I will be very very very sad... 😭",
+    "Ok fine, I will stop asking... 😢",
     "Just kidding, say yes please! ❤️"
 ];
 
@@ -16,14 +17,27 @@ let messageIndex = 0;
 function handleNoClick() {
     const noButton = document.querySelector('.no-button');
     const yesButton = document.querySelector('.yes-button');
-    
     noButton.textContent = messages[messageIndex];
     messageIndex = (messageIndex + 1) % messages.length;
-    
     const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
-    yesButton.style.fontSize = `${currentSize * 1.5}px`; // Increase Yes button size
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
 }
 
 function handleYesClick() {
-    window.location.href = "yes_page.html"; // Redirect to yes_page.html
+    window.location.href = "yes_page.html";
 }
+
+// Floating hearts animation
+function createHeart() {
+    const heart = document.createElement('div');
+    heart.classList.add('heart');
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.animationDuration = Math.random() * 2 + 3 + "s";
+    document.querySelector('.heart-container').appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 5000);
+}
+
+setInterval(createHeart, 500);
